aad4d53f9d7df76afbec53c950a52b06a40c8660
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-15 22:01:44 +0900

    Dad Joke added

--- day10-DadJokes/script.js
+++ day10-DadJokes/script.js
@@ -1,8 +1,39 @@
-const labels = document.querySelectorAll('.form-control label')
+const jokeEle = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+const title = document.getElementById('title')
 
-labels.forEach(label => {
-  label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-  .join('')
-})
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+// toggleTitle()
+
+// function toggleTitle() {
+//   if (title.innerHTML === 'Go through these jokes until you laugh 😊') {
+//     title.innerHTML = '笑ってはいけない英語学習'
+//   } else {
+//     title.innerHTML = 'Go through these jokes until you laugh 😊'
+//   }
+// }
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+  const res = await fetch('https://icanhazdadjoke.com', config)
+  const data = await res.json()
+  jokeEle.innerHTML = data.joke
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(res => res.json())
+//     .then(data => {
+//       jokeEle.innerHTML=data.joke
+//     })
+}

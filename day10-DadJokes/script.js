d7df86bd483c95616e9024dba6229d7145c9b912
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-17 13:15:24 +0900

    Fixed the bug-- sub-title not toggled

--- day10-DadJokes/script.js
+++ day10-DadJokes/script.js
@@ -1,29 +1,29 @@
 const jokeEle = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
-const title = document.getElementById('title')
+const titleEle = document.getElementById('title')
+
+const message1 = 'Can you laugh?'
+const message2 = 'Go through these jokes until you laugh'
 
 jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
-// toggleTitle()
 
-// function toggleTitle() {
-//   if (title.innerHTML === 'Go through these jokes until you laugh 😊') {
-//     title.innerHTML = '笑ってはいけない英語学習'
-//   } else {
-//     title.innerHTML = 'Go through these jokes until you laugh 😊'
-//   }
-// }
+function toggleTitle() {
+  titleEle.innerHTML = titleEle.textContent === message1 ? message2 : message1
+}
 
 async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
-  const res = await fetch('https://icanhazdadjoke.com', config)
-  const data = await res.json()
+    const config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
   jokeEle.innerHTML = data.joke
+
+  toggleTitle()
 
 // function generateJoke() {
 //   const config = {
